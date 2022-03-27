49063de847d4f53794dbc9dd3d9a1385456f4835
jdhasko <58209336+jdhasko@users.noreply.github.com>
2022-03-27 13:03:24 +0200

    Random date generator created

--- Personal Information Generator 2/Services/Generator.cs
+++ Personal Information Generator 2/Services/Generator.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Personal_Information_Generator_2
 {
-    internal class Generator
+    public static class Generator
     {
+        public static DateTime GetRandomBirthDate()
+        {
+            Random r = new Random();
+            int year = r.Next(1900, 2022);
+            int month = r.Next(0, 12);
+            int day = 0;
+            switch (month)
+            {
+                case 1: case 3: case 5: case 7: case 8: case 10: case 12: 
+                    day = r.Next(0, 31);
+                    break;
+                case 4: case 6: case 9: case 11:
+                    day = r.Next(0, 30);
+                    break;
+
+                case 2:
+                    day = r.Next(0, 29);
+                    break;
+            }
+            return new DateTime(year, month, day);
+        }
     }
 }
