4c536915904fe5169dec4038d33bb9212ac38a21
jdhasko <58209336+jdhasko@users.noreply.github.com>
2022-03-28 15:00:41 +0200

    A pointless test added

---
+++ PIG Test/GeneratorTest.cs
@@ -0,0 +1,25 @@
+﻿ using System;
+using Xunit;
+using Personal_Information_Generator_2;
+
+namespace PIG_Test
+{
+    public class GeneratorTest
+    {
+        [Theory]
+        [InlineData("male", "2000-11-09")]
+        public void GenerateFakeCpr_Test(string gender, DateTime birthDate)
+        {
+            //Arrange
+            string result;
+
+            //Act
+            Generator.GetFakeCPR(gender, birthDate);
+
+            //Assert
+            Assert.True(true);
+
+        }
+
+    }
+}
